21484ecc30f706842af60fa560a7c7d2eb8c259a
Alex Leakos <aleakos@gmail.com>
2022-02-28 18:43:40 -0700

    recieve response from cron

--- cronBackend/cronPushToken.js
+++ cronBackend/cronPushToken.js
@@ -1,24 +1,29 @@
 import cron from 'node-cron';
 import fetch from 'node-fetch';
+import moment from 'moment';
 
 const EXPO_CODE = 'ExponentPushToken[LeeHIYOY4XXC6yOa5e4lx6]'; // alex
 // const EXPO_CODE = 'ExponentPushToken[mgjDOXKcDE3hvd5cGhZMc7]'; // Jared
 
-cron.schedule('* * * * * *', function () {
-  console.log('running a task every second');
-  sendPushNotification().then(() => {
-    console.log('notification sent');
-  });
-});
+// cron.schedule('* * * * * *', function () {
+//   console.log('running a task every second');
+//   sendPushNotification().then(() => {
+//     console.log('notification sent');
+//   });
+// });
 
 async function sendPushNotification() {
   const message = {
     to: EXPO_CODE,
     sound: 'default',
-    title: 'Sent Via Cron Job',
-    body: 'And here is the body!',
-
-    data: { someData: 'goes here' },
+    title: 'Reminder Tile',
+    body: 'Reminder Body here...',
+    data: {
+      reminderID: '123456789',
+      reminderType: 'diet',
+      reminderTime: moment(new Date()).format('h:mm a').toString(), // replace with actual time in db
+      reminderDate: '2020-01-01',
+    },
   };
 
   const res = await fetch('https://exp.host/--/api/v2/push/send', {
@@ -32,3 +37,5 @@ async function sendPushNotification() {
   });
   console.log(res);
 }
+
+sendPushNotification();
