835e59d1c5a532e9db2545acbe632e6aadf8f9ad
Jared Kraus <jared.kraus3@gmail.com>
2022-03-02 16:08:03 -0700

    backend login logic

--- NHBackEnd/data/index.js
+++ NHBackEnd/data/index.js
@@ -2,7 +2,7 @@ const mysql2 = require('mysql2');
 const connection = mysql2.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'albessie',
+  password: 'Teck5Taillight!',
   database: 'NursingHackathon',
   dateStrings: true,
 });
